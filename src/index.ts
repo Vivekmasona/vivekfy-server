f41e2397a27a471536060ff9c42bd995c5656ee3
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-01 00:31:13 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -61,44 +61,7 @@ app.get('/download/audio', async (req, res) => {
   }
 })
 
-// Route for downloading video
-app.get('/download/video', async (req, res) => {
-  try {
-    const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
 
-    if (!videoURL) {
-      return res.status(400).send('Missing video URL');
-    }
-
-    // Get information about the video
-    const info = await ytdl.getInfo(videoURL);
-    const videoTitle = info.videoDetails.title;
-    const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Remove special characters from the title
-    const sanitizedTitle = autoTitle || 'video'; // Use the sanitized title or 'video' as a default
-
-    // Select the best available video format
-    const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
-
-    if (!format) {
-      return res.status(404).send('No suitable video format found');
-    }
-
-    // Get the content length (file size) of the video
-    const contentLength = format.contentLength;
-
-    // Set response headers to specify a downloadable video file with the auto-generated title
-    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}(vivek masona).mp4"`);
-    res.setHeader('Content-Type', 'video/mp4');
-    res.setHeader('Content-Length', contentLength); // Add content length to the headers
-
-    // Pipe the video stream into the response
-    ytdl(videoURL, { format }).pipe(res);
-
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
-  }
-})
 
 app.get('/video/:url', async (req: Request, res: Response) => {
   const urlParam = req.params.url

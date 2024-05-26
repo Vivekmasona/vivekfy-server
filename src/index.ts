c209b9355cf6ba8af64b253b115cfd5493ede405
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2024-05-26 20:31:50 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -138,34 +138,29 @@ app.get('/download/audio', async (req, res) => {
 // Route for downloading video
 app.get('/download/video', async (req, res) => {
   try {
-    const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
+    const videoURL = req.query.url;
 
     if (!videoURL) {
       return res.status(400).send('Missing video URL');
     }
 
-    // Get information about the video
     const info = await ytdl.getInfo(videoURL);
     const videoTitle = info.videoDetails.title;
-    const autoTitle = videoTitle.replace(/[^\w\s]/gi, ''); // Remove special characters from the title
-    const sanitizedTitle = autoTitle || 'video'; // Use the sanitized title or 'video' as a default
+    const sanitizedTitle = videoTitle.replace(/[^\w\s]/gi, '') || 'video';
 
-    // Select the best available video format
     const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
 
     if (!format) {
       return res.status(404).send('No suitable video format found');
     }
 
-    // Get the content length (file size) of the video
     const contentLength = format.contentLength;
+    const contentType = format.mimeType;
 
-    // Set response headers to specify a downloadable video file with the auto-generated title
-    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}(vivek masona).mp4"`);
-    res.setHeader('Content-Type', 'video/mp4');
-    res.setHeader('Content-Length', contentLength); // Add content length to the headers
+    res.setHeader('Content-Disposition', `attachment; filename="${sanitizedTitle}.mp4"`);
+    res.setHeader('Content-Type', contentType);
+    res.setHeader('Content-Length', contentLength);
 
-    // Pipe the video stream into the response
     ytdl(videoURL, { format }).pipe(res);
 
   } catch (error) {
@@ -173,6 +168,10 @@ app.get('/download/video', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 })
+
+
+
+
 app.get("/low-audio", async (req, res) => {
   const url = req.query.url;
   const itag = req.query.itag;
